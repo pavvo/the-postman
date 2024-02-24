30d4e76647dd8b47791582867298941a4203dcc0
pavle-odma <vv.pavle@gmail.com>
2024-02-24 09:50:18 +0100

    feat: add base layout
    Add base Layout component for wrapping all routes

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+import "./Layout.css";
+
+export function Layout() {
+  return (
+    <div className="layout">
+      <Outlet />
+    </div>
+  );
+}
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,15 @@
 import { useRoutes } from "react-router-dom";
 
+import { Layout } from "@/components/Layout";
+
 import { RoutePaths } from "./RoutePaths";
 
 export default function Router() {
   const element = useRoutes([
     {
       path: RoutePaths.ROOT,
-      element: <h1>Home</h1>,
+      element: <Layout />,
+      children: [{ index: true, element: <h1>Home</h1> }],
     },
   ]);
 
